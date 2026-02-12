7e1b7c646a6580bfa268e667a70c14dd074a88d1
imam baihaqi <imam.baihaqi@dwidasa.com>
2026-02-12 13:28:53 +0700

    umkm-service

---
+++ src/main/java/com/ib/umkm/UmkmApplication.java
@@ -0,0 +1,13 @@
+package com.ib.umkm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmkmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UmkmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ib/umkm/UmkmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ib.umkm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmkmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
